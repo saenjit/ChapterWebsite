6f9c59a4a956fa72879e3db8251cf7f8f2bf15b9
Todd Murphy <toddmurphy@todds-mbp.resource.campus.njit.edu>
2018-10-18 12:17:04 -0400

    added authentication to members.php

--- members.php
+++ members.php
@@ -1,3 +1,23 @@
+<!------------------------------AUTHENTICATION----------------------->
+        <?php
+            session_set_cookie_params(600);
+            session_start();
+            error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+            ini_set('display_errors' , 1);
+            
+            //check if authenticated
+            if (!$_SESSION['logged']){
+                echo"
+                <script>
+                    alert(\"Not logged in...\");
+                    window.location.replace(\"http://saenjit.com/memberlogin.html\");
+                </script>";
+                exit();
+            }
+
+        ?>
+<!------------------------------------------------------------------->
+
 <!Doctype html>
 <html lang="en">
 <head>
