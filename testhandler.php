86b5bd9b5bffdc1144453e38c24b13a13fb8e115
Todd Murphy <toddmurphy@todds-mbp.resource.campus.njit.edu>
2018-11-13 16:31:18 -0500

    more testing

--- testhandler.php
+++ testhandler.php
@@ -42,7 +42,14 @@
                 </script>";
                 exit();
             }
-            
+
+            $s = "select * from LoginTable where Email = '$email' and Active = 'no'" ;
+            $t = mysqli_query($db,$s) or die('<script type="text/javascript">
+                alert("Your account is already active! Please log in. If you forgot your password, please contact the ~Webmaster~!");
+                </script>
+                Error');
+
+            //Successfully passed all tests:
             echo"
             <script>
                 alert(\"Password Requested. Please check your email. Phi Alpha. \");
