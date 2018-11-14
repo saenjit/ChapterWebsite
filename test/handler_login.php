957661e28c890a45d08d8c72aef6be6fe2bc4a91
Todd Murphy <toddmurphy@Todds-MacBook-Pro.local>
2018-11-13 23:47:06 -0500

    added handler_login to handle login info

---
+++ test/handler_login.php
@@ -0,0 +1,66 @@
+<!------------------------------AUTHENTICATION----------------------->
+        <?php
+            session_set_cookie_params (600);
+            session_start();
+            error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+            ini_set('display_errors' , 1);
+
+
+            include ("sqlaccount.php") ;
+
+            $db = mysqli_connect($hostname, $username, $password, $project);
+
+            mysqli_select_db($db, $project); 
+
+            $email = $_POST['email'];
+            $pass = $_POST['password'];
+            $pass = md5($pass);
+            $_SESSION['email'] = $email;
+            $_SESSION['password'] = $pass;
+
+            $s = "select * from PasswordsForWebsite where Email = '$email' and Password = '$pass'" ;
+
+            $t = mysqli_query($db,$s);
+
+            $bad = false;
+
+            if (!isset ($pass)){
+                $bad = true;
+            }
+            if ($pass == ""){
+                $bad = true;
+            }
+
+            if ($bad){
+                echo "
+                <script>
+                    alert(\"Bad User/Password Character.\");
+                    window.location.replace(\"http://saenjit.com/memberlogin.html\");
+                </script>
+                ";
+                exit();
+            }
+
+            if (mysqli_num_rows($t)<1){
+                echo"
+                <script>
+                    alert(\"Incorrect Password...\");
+                    window.location.replace(\"http://saenjit.com/memberlogin.html\");
+                </script>";
+                exit();
+            }
+            
+            //Set logged to true cause they passed authentication
+            $_SESSION["logged"] = true;
+
+        echo"
+                <script>
+                    alert(\"Success\");
+                    window.location.replace(\"http://saenjit.com/memberlogin.html\");
+                </script>";
+                exit();
+
+        //header("location:members.php");
+
+        ?>
+<!------------------------------------------------------------------->
