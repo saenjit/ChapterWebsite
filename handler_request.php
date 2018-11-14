658230539902465d49d4a623f727594ea23e8f5c
Todd Murphy <toddmurphy@Todds-MacBook-Pro.local>
2018-11-14 01:32:48 -0500

    fixed mailer function

--- handler_request.php
+++ handler_request.php
@@ -64,14 +64,14 @@
             $s = "UPDATE LoginTable SET Active = 'first', Password = '$randompassHashed' WHERE Email = '$email'";
             mysqli_query ($db, $s) or die (mysqli_error($db));
 
-
+            $mail->IsHTML(true);
+            
             $out = "Welcome to SAE NJIT brother.<br>
             Your account is now active.<br>
-            Please sign in using your NJIT Email with temporary password: <b>" .$randompassword ."</b><br><br>When you log in for the first time, please change your password.<br><br>
+            Please sign in using your NJIT Email and with temporary password: <b>" .$randompassword ."</b><br><br>When you log in for the first time, please change your password.<br><br>
             Phi Alpha,<br>
             ~Webmaster~";
             
-
             $to = $email;
             $subject = "SAE NJIT Login Information";
             $message = $out;
