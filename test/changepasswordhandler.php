7c28341bab6bbcd572d187e5c4a6b87d6bd19db7
Todd Murphy <toddmurphy@Todds-MacBook-Pro.local>
2018-11-14 00:35:27 -0500

    more testing

--- test/changepasswordhandler.php
+++ test/changepasswordhandler.php
@@ -1,5 +1,7 @@
 <!------------------------------AUTHENTICATION----------------------->
         <?php
+            session_set_cookie_params(600);
+            session_start();
             error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
             ini_set('display_errors' , 1);
 
