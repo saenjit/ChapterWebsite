11110bb440b3e2e8347b902ec2afca94a3332b37
Todd Murphy <toddmurphy@Todds-MacBook-Pro.local>
2018-11-15 02:40:51 -0500

    added enable account function

---
+++ webmastertools/enableaccount.php
@@ -0,0 +1,33 @@
+<!------------------------------AUTHENTICATION----------------------->
+        <?php
+            session_set_cookie_params(600);
+            session_start();
+            error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+            ini_set('display_errors' , 1);
+            
+            include ("sqlaccount.php") ;
+
+            $db = mysqli_connect($hostname, $username, $password, $project);
+
+            mysqli_select_db($db, $project); 
+ 
+            //check if authenticated
+            if (!$_SESSION['webmaster']){
+                echo"
+                <script>
+                    alert(\"Not logged in...\");
+                    window.location.replace(\"http://saenjit.com/memberlogin.html\");
+                </script>";
+                exit();
+            }
+            $name = $_POST['enableAccountSelect'];
+
+        ?>
+<!------------------------------------------------------------------->
+<?php
+    $s = "UPDATE LoginTable SET Active = 'yes' WHERE Name = '$name'"; 
+    $t = mysqli_query($db,$s) or die("Enabling account failed.");
+    print("Successfully enabled account.");
+    sleep(5);
+    header("location:/webmaster.php");
+?>
