b1510efd5b336464287b9b480dc464bbf538ae3c
Todd Murphy <toddmurphy@todds-mbp.resource.campus.njit.edu>
2018-11-15 16:15:38 -0500

    added contact form

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -4,7 +4,8 @@
     $subject = $_POST['subject'];
     $message = $_POST['message'];
     
-    if (mail("saenjiotatau@gmail.com",$subject,$message)){
+    mail("saenjiotatau@gmail.com",$subject,$message);
+    if ($test){
         echo "
         <script>
             alert(\"Message Sent. Allow 24-48 hours for us to get back to you.\");
