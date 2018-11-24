5417112b4102dada6f70ab7705fb572376d1ea81
Todd Murphy <toddmurphy@Todds-MacBook-Pro.local>
2018-11-24 00:14:22 -0500

    testing access to documents

--- members.php
+++ members.php
@@ -189,7 +189,9 @@
                 <h4>Documents/Links</h4>
                 <ul>
                   <li>
-                    <a href="<?php readfile("/documents/BudgetF18.pdf");  ?>">Budget</a>
+                    <a href="<?php header('Content-disposition: attachment; filename= BudgetF18.pdf');
+                                   header('Content-type: application/pdf');
+                                   readfile("/documents/BudgetF18.pdf");  ?>">Budget</a>
                   </li>
                   <li>
                     <a href="/documents/FundraisingF18.pdf">Fundraising</a>
