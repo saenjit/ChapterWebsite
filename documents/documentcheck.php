644410b43c7d29ba072d3708608d5b78ba186a04
Todd Murphy <toddmurphy@Todds-MacBook-Pro.local>
2018-11-24 00:47:43 -0500

    testing access to documents

--- documents/documentcheck.php
+++ documents/documentcheck.php
@@ -23,12 +23,6 @@
             $document = $_SESSION['document'];
             
             $file = $document;
-            $filename = $document;
-            header('Content-type: application/pdf');
-            header('Content-Disposition: inline; filename="' . $filename . '"');
-            header('Content-Transfer-Encoding: binary');
-            header('Content-Length: ' . filesize($file));
-            header('Accept-Ranges: bytes');
             @readfile($file);
 
         ?>
