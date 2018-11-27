402f674e517264154123312031ea8f4ae7ad820e
Todd Murphy <toddmurphy@todds-mbp.resource.campus.njit.edu>
2018-11-27 13:36:00 -0500

    emails now send to recruitment chair which is labeled in the database

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -1,15 +1,29 @@
 <?php
+    include ("sqlaccount.php");
+
+    $db = mysqli_connect($hostname, $username, $password, $project);
+
+    mysqli_select_db($db, $project); 
+
+    $s = "select * from LoginTable where Position = 'R'" ;
+
+    $t = mysqli_query($db,$s);
+
+    while ( $r = mysqli_fetch_array($t,MYSQLI_ASSOC) ) {
+        $sendToEmail                   = $r[ "Email" ];
+    }
+
     $name = $_POST['name'];
     $email = $_POST['email'];
     
     $subject = "SAENJIT.com Contact Form: ";
     $subject .= $_POST['subject'];
 
-    $message = "//Contact Form from saenjit.com: // ";
+    $message = "--Contact Form from saenjit.com: --   ";
     $message.= $_POST['message'];
     $message.=" (This was sent from ".$email.".)";
     
-    if (mail("tam44@njit.edu", $subject, $message)){
+    if (mail($sendToEmail, $subject, $message)){
         echo "
         <script>
             alert(\"Message Sent. Allow 24-48 hours for us to get back to you.\");
