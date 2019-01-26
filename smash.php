f1e8d773d2c12ffcfafc5c95cc69620cc0b87c76
Todd Murphy <tmurphy605@icloud.com>
2019-01-25 21:51:36 -0500

    fixed titles

--- smash.php
+++ smash.php
@@ -51,7 +51,7 @@ $rankLoser='Loser';
 <html lang="en">
 <head>
   <meta charset="utf-8">
-  <title>ΣAE - NJIT | Members Only</title>
+  <title>ΣAE - NJIT | Smash Portal</title>
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
   <meta content="" name="keywords">
   <meta content="" name="description">
--- smashentry.php
+++ smashentry.php
@@ -42,7 +42,7 @@
 <html lang="en">
 <head>
   <meta charset="utf-8">
-  <title>ΣAE - NJIT | Members Only</title>
+  <title>ΣAE - NJIT | Smash Entry</title>
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
   <meta content="" name="keywords">
   <meta content="" name="description">
